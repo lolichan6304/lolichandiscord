f75af9223d67492d6361e9f13c4eceb0d3df030e
lolichan6304 <lolichan6304@gmail.com>
2020-11-20 12:01:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ load_dotenv()
 
 # Load env
 TOKEN = os.getenv('DISCORD_TOKEN')
-if os.environ.get('TAGS_NHENTAI') is None:
-    os.environ['TAGS_NHENTAI'] = ""
-if os.environ.get('ALLOWED_ROLES') is None:
-    os.environ['ALLOWED_ROLES'] = ""
 
 client = LoliChan(verbose=False)
 
